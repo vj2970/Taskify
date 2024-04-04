8136eabfaabaf7f9f580bb463f1744345b64835f
vj2970 <99133919+vj2970@users.noreply.github.com>
2024-04-04 16:11:43 +0530

    Backend Files

---
+++ Backend/taskify/src/main/java/com/kpit/taskify/entity/User.java
@@ -0,0 +1,29 @@
+package com.kpit.taskify.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "USER_DETAILS")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String first_name;
+    private String last_name;
+    private String email;
+    private String pass;
+
+}
